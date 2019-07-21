902591ac20b7184c56918182b9034f90b4e68704
KloeAnn <201530611012@mail.scut.edu.cn>
2019-07-21 16:59:06 +0800

    feat:add Class CaseInformation

---
+++ src/main/java/com/tw/apistackbase/model/CaseInformation.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class CaseInformation {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "objectiveDescript", nullable = false)
+    private String objectiveDescript;
+
+    @Column(name = "subjectiveDescript", nullable = false)
+    private String subjectiveDescript;
+
+    public CaseInformation() {
+    }
+
+    public CaseInformation(String objectiveDescript, String subjectiveDescript) {
+        this.objectiveDescript = objectiveDescript;
+        this.subjectiveDescript = subjectiveDescript;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getObjectiveDescrip() {
+        return objectiveDescript;
+    }
+
+    public void setObjectiveDescript(String objectiveDescript) {
+        this.objectiveDescript = objectiveDescript;
+    }
+
+    public String getSubjectiveDescript() {
+        return subjectiveDescript;
+    }
+
+    public void setSubjectiveDescript(String subjectiveDescript) {
+        this.subjectiveDescript = subjectiveDescript;
+    }
+}
